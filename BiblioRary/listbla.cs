2c56a6de67acf545a993ba0b1dd11cda79d8d436
Tilly Brito <Gilary_FB@live.nl>
2018-12-09 20:08:05 +0100

    added list

---
+++ BiblioRary/listbla.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace BiblioRary
+{
+    class listbla
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string>();
+
+            foreach (var s in list)
+            {
+
+            }
+            foreach (var s in MyList())
+            {
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+
+    private static IEnumberable<string> MyList()
+    {
+        var client = new HttpClient();
+        var result = client.GetStringAsync("www.google.com").Result;
+        yield return result;
+    }
+}
